0cf45a000207b56453ac249822794698499ab506
quiquealvarado <jalvnun309@g.educaand.es>
2023-10-02 19:48:49 +0200

    Cambios en id de cartones y en generarNumero()

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-//Variables necesarias
+// Variables necesarias
 let jugador = [];
 let maquina = [];
 let numerosTotal = [];
 
-//Generar array de numeros del 1 (min) al 90 (max)
+// Generar array de numeros del 1 (min) al 90 (max)
 function generarArray(min, max) {
   const array = [];
   for (let i = min; i <= max; i++) {
@@ -12,8 +12,8 @@ function generarArray(min, max) {
   return array;
 }
 
-//Numeros aleatorios
-function Numeros90(array,cantidad) {
+// Numeros aleatorios
+function Numeros90(array, cantidad) {
   const nums = [];
   const copiaNums = array.slice();
 
@@ -26,16 +26,35 @@ function Numeros90(array,cantidad) {
   return nums;
 }
 
-//Generar cartones
+// Generar cartones
 function aJugar() {
-  const jugador = Numeros90(generarArray(1, 90), 15);
-  const maquina = Numeros90(generarArray(1, 90), 15);
+  jugador = Numeros90(generarArray(1, 90), 15); // Usar las variables globales en lugar de declarar nuevas
+  maquina = Numeros90(generarArray(1, 90), 15); // Usar las variables globales en lugar de declarar nuevas
 
   const res1 = document.getElementById("jugador");
   const res2 = document.getElementById("maquina");
 
-  res1.innerHTML = '' + jugador.join(', ');
-  res2.innerHTML = '' + maquina.join(', ');
+  function formatearCarton(numeros) {
+    let cartonHTML = '<table>';
+    let cellCounter = 0; // Contador para los identificadores de las celdas
+
+    for (let i = 0; i < 3; i++) {
+      cartonHTML += '<tr>';
+      for (let j = 0; j < 5; j++) {
+        const numero = numeros[i * 5 + j];
+        const cellId = `cell${cellCounter}`; // Crear un identificador único
+        cartonHTML += `<td id="${cellId}">${numero}</td>`;
+        cellCounter++;
+      }
+      cartonHTML += '</tr>';
+    }
+    cartonHTML += '</table>';
+    return cartonHTML;
+  }
+
+
+  res1.innerHTML = formatearCarton(jugador);
+  res2.innerHTML = formatearCarton(maquina);
   jugar.style.display = 'none';
   generar.style.display = 'block';
 
@@ -62,16 +81,28 @@ function generarNumero() {
   const randomNumEstilo = document.getElementById("resultados2");
   randomNumEstilo.innerText = 'Último Número generado: ' + randomNum;
 
-  if (jugador.includes(randomNum)) {
-    randomNumEstilo.classList.add('numCoincidente');
-  } else if (maquina.includes(randomNum)) {
-    randomNumEstilo.classList.add('numCoincidente');
-  } else {
-    randomNumEstilo.classList.remove('numCoincidente');
+  const seEncontro = jugador.includes(randomNum);
+
+  if (seEncontro) {
+    const indice = jugador.indexOf(randomNum);
+    jugador[$indice].classList.add('numCoincidente');
   }
 }
+/*
+* if (seEncontro) {
+  const indice = miArray.indexOf(elementoBuscado);
+  console.log(`El elemento ${elementoBuscado} se encuentra en la posición ${indice}`);
+} else {
+  console.log(`El elemento ${elementoBuscado} no se encuentra en el array.`);
+}
+*
+*
+* */
+
 
 function refreshNumeros() {
   const res = document.getElementById("resultados");
   res.innerHTML = 'Números Generados: ' + numerosTotal.join(', ');
 }
+
+
